ae4f0682c4479e61ff0866dd76fec51c2696f7db
Scott Juneau <dsjuneau@gmail.com>
2019-08-10 07:54:55 -0500

    trying again

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/client/build"));
+  app.use(express.static("client/build"));
 }
 
 mongoose
