04c8114ee60ef44a866b90640e7fb5a361861b08
Scott Juneau <dsjuneau@gmail.com>
2019-08-10 07:46:05 -0500

    changed something

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <p>Testing 1, 2, 3, ...</p>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Something</p>
+      </div>
+    );
+  }
 }
 
 export default App;
